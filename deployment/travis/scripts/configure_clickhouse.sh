0166d37899861f9bd4c265f677993c90a00f8000
Naumov Konstantin <naumov-web@yandex.ru>
2019-10-17 17:05:08 +0300

    Replace clickhouse main config

--- deployment/travis/scripts/configure_clickhouse.sh
+++ deployment/travis/scripts/configure_clickhouse.sh
@@ -2,6 +2,8 @@
 
 echo "Configure clickhouse server!"
 
+sudo rm /etc/clickhouse-server/config.xml
+sudo cp ./deployment/travis/configs/clickhouse-main.xml /etc/clickhouse-server/config.xml
 sudo cp ./deployment/travis/configs/clickhouse-user.xml /etc/clickhouse-server/users.d/local_user.xml
 
 sudo service clickhouse-server restart
